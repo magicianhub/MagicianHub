225b097f9c320da2e93abf64a06dc5d4486218dd
Pavel Erokhin <MairwunNx@gmail.com>
2019-07-07 22:57:03 +0300

    Added base sources of MagicianHub.Tests project.

---
+++ MagicianHub/MagicianHub.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MagicianHub.Tests
+{
+    internal class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
