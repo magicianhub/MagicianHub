8473e79a82ced1d835ec313d76f8f556c0f2991a
Pavel Erokhin <MairwunNx@gmail.com>
2019-07-08 16:18:22 +0300

    MainPage renamed to BasePage.

--- MagicianHub/MagicianHub/App.xaml.cs
+++ MagicianHub/MagicianHub/App.xaml.cs
@@ -4,6 +4,7 @@ using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using MagicianHub.Views;
 
 namespace MagicianHub
 {
@@ -56,7 +57,7 @@ namespace MagicianHub
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(BasePage), e.Arguments);
                 }
 
                 // Ensure the current window is active
---
+++ MagicianHub/MagicianHub/Views/BasePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicianHub.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BasePage
+    {
+        public BasePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
