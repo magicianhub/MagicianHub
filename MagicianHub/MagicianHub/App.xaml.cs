b9bb528c1cb8f21b087a660189d5a648fb72126b
Pavel Erokhin <MairwunNx@gmail.com>
2019-07-08 16:15:57 +0300

    MainPage moved to Views/MainPage.

--- MagicianHub/MagicianHub/App.xaml.cs
+++ MagicianHub/MagicianHub/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MagicianHub
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -18,8 +18,8 @@ namespace MagicianHub
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
+            Suspending += OnSuspending;
         }
 
         /// <summary>
